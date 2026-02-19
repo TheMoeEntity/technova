dfe69bf61fe09b0ed26f62ac0105c112478402d8
Moses Nwigberi <mosesnwigberi@gmail.com>
2026-02-19 12:42:43 +0100

    feat: Replace `PartnersMarquee` component with `Partners` component

--- src/app/(routes)/page.tsx
+++ src/app/(routes)/page.tsx
@@ -2,15 +2,17 @@ import Hero from "@/component/sections/Home/Hero";
 import About from "@/component/sections/Home/About";
 import Attendees from "@/component/sections/Home/Attendees";
 import FAQSection from "@/component/sections/Home/FAQ";
-import PartnersMarquee from "@/component/sections/Home/PartnersMarquee";
+// import PartnersMarquee from "@/component/sections/Home/PartnersMarquee";
 import { WhoShouldAttend } from "@/component/sections/Home/WhoShouldAttend";
+import Partners from "@/component/sections/Home/Partners";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-white font-bricolage-grotesque">
       <Hero />
       <About />
-      <PartnersMarquee />
+      {/* <PartnersMarquee /> */}
+      <Partners />
       <Attendees />
       <WhoShouldAttend />
       <FAQSection />
